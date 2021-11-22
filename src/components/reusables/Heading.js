e0b6d393d143c54678205050b8df07f45535a92f
ashwin <ashwinkumartth@gmail.com>
2021-11-22 13:28:14 +0530

    heading component created

---
+++ src/components/reusables/Heading.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Heading = (props) => {
+    const { className, type, title } = props
+
+    let heading
+    if(type === 'h1') {
+        heading = <h1 className={ className }>{ title }</h1>
+    } else if(type === 'h2') {
+        heading = <h2 className={ className }>{ title }</h2>
+    } else if(type === 'h3') {
+        heading = <h3 className={ className }>{ title }</h3>
+    }
+
+    return heading
+}
+
+export default Heading
